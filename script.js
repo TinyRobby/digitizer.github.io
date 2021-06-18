6b5feab445bde6c7d00a4b5953bda77142cfee14
Robby <77552033+TinyRobby@users.noreply.github.com>
2021-06-18 15:12:07 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function siteAlert() {
+    alert('This site is under construction. Sorry!');
+    const message = 'Please go to "http://robbyswebsite.wordpress.com/digitizer"';
+    alert(message);
+    console.log(message); 
+}
+siteAlert();
